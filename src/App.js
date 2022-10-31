96e56e0a73889331620ef75a3fea4b1eac040f45
Eazy-creator <82000640+Eazy-creator@users.noreply.github.com>
2022-10-31 21:37:28 +0000

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,122 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+import countries from 'i18n-iso-countries';
+
+countries.registerLocale(require('i18n-iso-countries/langs/en.json'));
+
+function App() {
+  // State
+  const [apiData, setApiData] = useState({});
+  const [getState, setGetState] = useState('Enter Location');
+  const [state, setState] = useState('London');
+
+  // API KEY AND URL
+  const apiKey = '3d277b1a958f008410870bde140b771d';
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=${apiKey}`;
+
+  // Side effect
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => setApiData(data));
+  }, [apiUrl]);
+
+  const inputHandler = (event) => {
+    setGetState(event.target.value);
+  };
+
+  const submitHandler = () => {
+    setState(getState);
+  };
+
+  const kelvinToFarenheit = (k) => {
+    return (k - 273.15).toFixed(2);
+  };
+
+  return (
+    <div className="App">
+      <header className="d-flex justify-content-center align-items-center text-white">
+        <h2>Weather API </h2>
+      </header>
+      <div className="container">
+        <div className="mt-3 d-flex flex-column justify-content-center align-items-center">
+          <div class="col-auto">
+            <label for="location-name" class="col-form-label">
+
+            </label>
+          </div>
+          <div class="col-auto">
+            <input
+              type="text"
+              id="location-name"
+              class="form-control"
+              onChange={inputHandler}
+              value={getState}
+            />
+          </div>
+          <button className="text-dark btn btn-primary mt-4 bg-white" onClick={submitHandler}>
+            Search
+          </button>
+        </div>
+
+        <div>
+          {apiData.main ? (
+            <div class="card-body text-center">
+              <img
+                src={`http://openweathermap.org/img/w/${apiData.weather[0].icon}.png`}
+                alt="weather status icon"
+                className="weather-icon"
+              />
+
+              <p className="h2 text-white">
+                {kelvinToFarenheit(apiData.main.temp)}&deg; C
+              </p>
+
+              <p className="h5">
+                <i className="fas fa-map-marker-alt"></i>{' '}
+                <strong>{apiData.name}</strong>
+              </p>
+
+              <div className="row mt-4">
+                <div className="col-md-6">
+                  <p className="text-white">
+                    <i class="fas fa-temperature-low text-white"></i>{' '}
+                    <strong className="text-white">
+                      {kelvinToFarenheit(apiData.main.temp_min)}&deg; C
+                    </strong>
+                  </p>
+                  <p>
+                    <i className="fas fa-temperature-high text-white"></i>{' '}
+                    <strong className="text-white">
+                      {kelvinToFarenheit(apiData.main.temp_max)}&deg; C
+                    </strong>
+                  </p>
+                </div>
+                <div className="col-md-6 text-white">
+                  <p>
+                    {' '}
+                    <strong className="text-white">{apiData.weather[0].main}</strong>
+                  </p>
+                  <p className="text-white">
+                    <strong className="text-white">
+                      {' '}
+                      {countries.getName(apiData.sys.country, 'en', {
+                        select: 'official',
+                      })}
+                    </strong>
+                  </p>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <h1>Loading</h1>
+          )}
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
